dd5d097a137a12a62d1d46f0334bc57e1b47d0f7
inlapidem <129763175+inlapidem@users.noreply.github.com>
2023-05-13 10:01:23 +0900

    Update memo.py

--- memo.py
+++ memo.py
@@ -1,21 +1,7 @@
-'''hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-bound_lower = np.array([25, 20, 20])
-bound_upper = np.array([100, 255, 255])
-
-mask_green = cv2.inRange(hsv_img, bound_lower, bound_upper)
-kernel = np.ones((7,7),np.uint8)
-
-mask_green = cv2.morphologyEx(mask_green, cv2.MORPH_CLOSE, kernel)
-mask_green = cv2.morphologyEx(mask_green, cv2.MORPH_OPEN, kernel)
-
-seg_img = cv2.bitwise_and(img, img, mask=mask_green)
-contours, hier = cv2.findContours(mask_green.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-output = cv2.drawContours(seg_img, contours, -1, (0, 0, 255), 3)
-
 def onMouse(event, x, y, flags, param):
     if event == cv2.EVENT_LBUTTONDOWN:
         print('왼쪽 마우스 클릭 했을 때 좌표 : ', x, y)
-cv2.setMouseCallback('img', onMouse)'''
+cv2.setMouseCallback('img', onMouse)
 
 # !/usr/bin/env python3
 import rospy
@@ -99,9 +85,9 @@ if __name__ == '__main__':
     rospy.spin()
    
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            adaptive_threshold = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
-            contours, _ = cv2.findContours(adaptive_threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+adaptive_threshold = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+contours, _ = cv2.findContours(adaptive_threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
             
-            img_copy = img.copy()
-            cv2.drawContours(image = img_copy, contours = contours, contourIdx = -1, color = (0, 255, 0), thickness = 2, lineType = cv2.LINE_AA)
-            cv2.imshow('Image', img_copy)
+img_copy = img.copy()
+cv2.drawContours(image = img_copy, contours = contours, contourIdx = -1, color = (0, 255, 0), thickness = 2, lineType = cv2.LINE_AA)
+cv2.imshow('Image', img_copy)
