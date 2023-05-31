8962c1aa5ed2bc3beda78a04a60f2d57583d7791
inlapidem <129763175+inlapidem@users.noreply.github.com>
2023-05-31 22:02:42 +0900

    Update new.py

--- new.py
+++ new.py
@@ -12,17 +12,21 @@ class DetermineColor:
         self.image_sub = rospy.Subscriber('/camera/color/image_raw', Image, self.callback)
         self.color_pub = rospy.Publisher('/rotate_cmd', Header, queue_size=10)
         self.bridge = CvBridge()
+        #ㄱㅖㅅㅗㄱ ㅆㅓㅇㅑ ㅎㅏㄴㅡㄴ ㄷㅔㅇㅣㅌㅓ ㅈㅓㅈㅏㅇ
         self.count = 0
-        self.tmpval = []
-        self.txt = '0'
-        self.diff = []
+        self.tmpval = [] #ㅇㅣㅈㅓㄴ ㅍㅡㄹㅔㅇㅣㅁㅇㅔㅅㅓㅇㅡㅣ ㅍㅣㄱㅅㅔㄹ
+        self.txt = '0' #ㅎㅗㅣㅈㅓㄴ ㄱㅕㄹㅈㅓㅇㅎㅏㄴㅡㄴ ㅌㅔㄱㅅㅡㅌㅡ
+        self.diff = [] #ㅂㅕㄴㅎㅗㅏㅎㅏㄴ ㅍㅣㄱㅅㅔㄹㄷㅡㄹㅇㅡㅣ ㅁㅗㅇㅡㅁ
         self.initarr = []
 
     def callback(self, data):
         try:
             # listen image topic
             img = self.bridge.imgmsg_to_cv2(data, 'bgr8')
+            #ㅇㅣㅁㅣㅈㅣ ㅅㅏㅇㅣㅈㅡ ㅅㅓㄹㅈㅓㅇ
             h, w, c = img.shape
+            
+            #ㅍㅣㄱㅅㅔㄹ ㅅㅜ ㅅㅔㄹ ㄸㅐ ㅍㅣㄹㅇㅛㅎㅏㄴ ㅂㅕㄴㅅㅜ ㅅㅓㄹㅈㅓㅇ
             etc = 0
             blue = 0
             red = 0
@@ -43,18 +47,19 @@ class DetermineColor:
             val = []
             tmpdiff = []
             
-            if len(self.tmpval) == 0:
+            if len(self.tmpval) == 0: #ㅊㅓㅅ ㅎㅗㅏㅁㅕㄴ
                 for i in range(0, h, 15):
                     tmparr = []
                     for j in range(0, w, 15):
                         _tmp = list(img[i, j])
                         tmparr.append(_tmp)
                         if sum(_tmp) <= 150:
-                            blackarr.append([i, j])
-                    val.append(tmparr)
+                            blackarr.append([i, j]) #rgbㄱㅏㅂㅅㅇㅡㄹ ㅂㅏㅌㅏㅇㅇㅡㄹㅗ ㄱㅓㅁㅇㅡㄴㅅㅐㄱ ㅍㅣㄱㅅㅔㄹ(ㅎㅗㅏㅁㅕㄴ ㅌㅔㄷㅜㄹㅣ)ㄹㅡㄹ ㅍㅏㅈㅇㅡㅁ
+                    val.append(tmparr) 
                         
-                self.tmpval = val
+                self.tmpval = val #ㅍㅣㄱㅅㅔㄹ ㅈㅓㅈㅏㅇㅎㅏㄴㅡㄴ ㄱㅗㅅㅇㅔ ㅈㅣㅂㅇㅓㄴㅓㅎㅇㅡㅁ
                 
+                #ㅁㅗㄷㅡㄴ ㄱㅓㅁㅇㅡㄴㅅㅐㄱ ㅍㅣㄱㅅㅔㄹㄷㅡㄹㅇㅡㅣ ㅍㅕㅇㄱㅠㄴㅇㅡㄹ ㅈㅜㅇㄱㅏㄴㄱㅏㅂㅅㅇㅡㄹㅗ ㅈㅣㅈㅓㅇ. ㄱㅡ ㄱㅓㄴㅇㅡㄴㅅㅔㄱ ㅍㅣㄱㅅㅔㄹ ㅈㅗㅏㅇㅜㄹㅗ ㄱㅓㅁㅇㅡㄴㅅㅐㄱ ㄱㅏㅈㅏㅇ ㄱㅏㄲㅏㅇㅜㄴ ㅍㅣㄱㅅㅔㄹㅇㅡㄹ ㅅㅓㄴㅈㅓㅇㅎㅐ ㅈㅣㄱㅅㅏㄱㅏㄱㅎㅕㅇㅇㅡㄹ ㅁㅏㄴㄷㅡㄹㅇㅓ ㄱㅡ ㅇㅏㄴㅇㅔ ㅇㅣㅆㄴㅡㄴ ㅍㅣㄱㅅㅔㄹㄷㅡㄹ rgbㄹㅡㄹ ㅍㅏㄴㄷㅏㄴㅎㅐ ㅊㅗㄱㅣ ㅎㅗㅣㅈㅓㄴ ㅂㅏㅇㅎㅑㅇㅇㅡㄹ ㅅㅓㄹㅈㅓㅇㅎㅏㄴㄷㅏ.
                 x = 0
                 y = 0
                 for dot in blackarr:
@@ -78,6 +83,7 @@ class DetermineColor:
                 yinit = y - abs(xmin - y)
                 yfin = y + abs(xmin - y)
                 
+                #rgbㄹㅡㄹ ㅍㅏㄴㄷㅏㄴㅎㅐ ㅃㅏㄹㄱㅏㄴㅅㅐㄱ, ㅍㅏㄹㅏㄴㅅㅐㄱ, ㄴㅏㅁㅓㅈㅣㄹㅡㄹ ㅍㅏㄴㄷㅏㄴㅎㅏㄴㅡㄴ ㅋㅜㄷㅡ
                 for i in range(xinit, xfin, 15):
                     for j in range(yinit, yfin, 15):
                         tmp = list(img[i, j])
@@ -95,7 +101,7 @@ class DetermineColor:
                                     etc += 1
                             else:
                                 etc += 1
-                _max = max([blue, red, etc])
+                _max = max([blue, red, etc]) #ㅊㅗㅣㅂㅣㄴㄱㅏㅂㅅㅇㅡㄹ ㅛㅐㅅㅇㅡㄹㅗ ㅅㅓㄹㅈㅓㅇ
                 if _max == blue:
                     self.txt = '+1'
                 elif _max == red:
@@ -104,21 +110,23 @@ class DetermineColor:
                     self.txt = '0'
                         
                 
-            else:
+            else: #ㅇㅏㅍㅇㅔ ㅎㅗㅏㅁㅕㄴㅇㅣㅇㅣㅆㅇㅓㅆㄷㅓㄴ ㄱㅕㅇㅇㅜ
                 for i in range(0, h, 15):
                     tmparr = []
                     for j in range(0, w, 15):
                         tmp = list(img[i, j])
                         ttmp = list(self.tmpval[int(i / 15)][int(j / 15)])
                         
+                        #ㅇㅣㅈㅓㄴㄱㅗㅏ ㅂㅣㄱㅛㅎㅐ ㅇㅓㄹㅁㅏㄴㅏㅁㅏㄴㅎㅇㅣ ㄷㅏㄹㄹㅏㅈㅕㅆㄴㅡㄴㅈㅣㄹㅡㄹ ㅍㅏㄴㄷㅏㄴㅎㅏㄴㄷㅏ
                         if (abs(int(tmp[0]) - int(ttmp[0])) + abs(int(tmp[1]) - int(ttmp[1])) + abs(int(tmp[2]) - int(ttmp[2]))) >= 200:
                             tmpdiff.append([i, j])
                             
                         tmparr.append(tmp)
                     val.append(tmparr)
                     
-                if len(tmpdiff) > len(self.diff):
+                if len(tmpdiff) > len(self.diff): # ㅂㅏㄲㅜㅣㄴ ㅂㅜㅂㅜㄴㅇㅣ ㄷㅓ ㅁㅏㄴㅎㄷㅏㅁㅕㄴ ㅎㅐㄷㅏㅇ ㅂㅏㄲㅜㅣㄴ ㅂㅜㅂㅜㄴㅇㅡㄹ ㅁㅗㄴㅣㅌㅓ ㅇㅕㅇㅇㅕㄱㅇㅡㄹㅗ ㅅㅓㄹㅈㅓㅇ
                     self.diff = tmpdiff
+                    #ㅇㅕㄱㅣㅅㅓ ㅁㅜㄴㅈㅔㅈㅓㅁㅇㅣ ㅂㅏㄹㅅㅐㅇ->ㅈㅣㄴㄷㅗㅇㅅㅜ ㄸㅐㅁㅜㄴㅇㅔ ㄲㅗㅁㅅㅜㄹㅗ ㅂㅏㄲㅜㅣㄴ ㅍㅣㄱㅅㅔㄹ ㅇㅛㅅㅗㄹㅡㄹ ㅇㅣㄹㅇㅣㄹㅇㅣ ㅂㅣㄱㅛㅎㅐㅅㅓ ㅂㅜㅈㅗㄱㅎㅏㄴ ㅂㅜㅂㅜㄴㅇㅡㄹ ㅊㅐㅇㅜㅓㄴㅓㅎㄴㅡㄴㄱㅔ ㅇㅏㄴㅣㄹㅏ ㅌㅗㅇㅉㅐㄹㅗ ㅂㅕㄴㄱㅕㅇ. ㅇㅣ ㅂㅜㅂㅜㄴㅇㅣ ㄱㅐㅅㅓㄴㄷㅗㅣㄹ ㅅㅜ ㅇㅣㅆㄸㅏㄱㅗ ㅅㅐㅇㄱㅏㄱㅎㅏㅁ.
                 for arr in self.diff:
                     tmp = list(img[arr[0], arr[1]])
                     if tmp[0] >= 200 and tmp[1] >=200 and tmp[2] >= 200:
@@ -137,7 +145,7 @@ class DetermineColor:
                             etc += 1
                     
                                    
-                if len(tmpdiff) > 10:
+                if len(tmpdiff) > 10: #ㅁㅏㄴㅎㅇㅣ ㅂㅏㄲ ㅣㅇㅓㅆㄷㅏㅁㅕㄴ ㅅㅐㄱㅅㅏㅇ ㅂㅕㄴㄱㅕㅇ
                     _max = max([blue, red, etc])
                     if _max == blue:
                         self.txt = '+1'
